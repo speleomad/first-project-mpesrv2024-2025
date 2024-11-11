f0f5f40a56aa68276df74d3dd8de9572c0641c28
arbitrontunisie <arbitrontunisie@gmail.com>
2024-11-11 19:30:17 +0100

    routing commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"about", component:AboutComponent},
+  {path:"contacts",component:ContactsComponent},
+  {path:"", redirectTo:"about", pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
